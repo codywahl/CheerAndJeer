a8c25fbb3a83befdadcb96dc9af7eb9495dba80a
Cody Wahl <Cody.Wahl@greenwayhealth.com>
2015-06-10 13:42:49 -0400

    Comments

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+//Using express
 var express = require('express');
 var app express();
 
+//Defining GET route for /cheer.txt
 app.get('/cheer.txt', function(request, response) {
 	response.end('You have quite lovely eyes.');
 });
 
+//Defining GET route for /jeer.txt
 app.get('/jeer.txt', function(request, response) {
 	response.end('Your mother was indeed a hampster.');
 });
+
 
 var server = app.listen(8080, function() {
 	console.log('Listening on port 8080');
